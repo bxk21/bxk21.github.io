553b052f976fe0de77790d003f2f3a1883c4aa21
Bo Kang <bkang@amyx.com>
2025-02-13 09:24:37 -0500

    Fix Metadata

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	output: "export",
-	basePath: "/bxk21.github.io",
+	// basePath: "/bxk21.github.io",
+	// trailingSlash: true,
 };
 
 export default nextConfig;
