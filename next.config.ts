4efcb201e13c4677ff32b4ce6c8ce85d1a705162
Bo Kang <bkang@amyx.com>
2025-02-13 10:52:12 -0500

    config adjustments

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	output: "export",
-	distDir: '/out',
-	// basePath: "/bxk21.github.io",
-	// trailingSlash: true,
+	distDir: 'out',
+	basePath: "/bxk21.github.io",
+	trailingSlash: true,
 };
 
 export default nextConfig;
