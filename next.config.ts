28b171772d080f422c397b3d11c9ec3c65d81b6f
Bo Kang <bkang@amyx.com>
2025-02-13 10:34:23 -0500

    change read dir

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	output: "export",
-	distDir: 'out',
+	distDir: '/out',
 	// basePath: "/bxk21.github.io",
 	// trailingSlash: true,
 };
