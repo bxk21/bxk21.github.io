3c5d242a4ce60f2b8de1b9826d03def16d786b62
Bo Kang <bkang@amyx.com>
2025-02-13 11:06:26 -0500

    remove next config for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	output: "export",
-	distDir: 'out',
-	basePath: "/bxk21.github.io",
-	trailingSlash: true,
+	// output: "export",
+	// distDir: 'out',
+	// basePath: "/bxk21.github.io",
+	// trailingSlash: true,
 };
 
 export default nextConfig;
